b760934a770e1f2511b576f80253942704a477f8
liuchifong666 <phonggl2207@domain.com>
2025-10-02 10:55:42 +0700

    first commit

---
+++ PS43755_slide4/src/main/java/poly/edu/Ps43755Slide4Application.java
@@ -0,0 +1,13 @@
+package poly.edu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ps43755Slide4Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ps43755Slide4Application.class, args);
+	}
+
+}
---
+++ PS43755_slide4/src/test/java/poly/edu/Ps43755Slide4ApplicationTests.java
@@ -0,0 +1,13 @@
+package poly.edu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ps43755Slide4ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
